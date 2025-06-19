272799e5db8c5f1cf192fe14d7b39b268f46af53
Laurence Unabia <laurenceunabia@umbradigitalcompany.com>
2025-06-20 00:38:02 +0800

    installed cors on backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 4000;
+
+app.use(cors()); // Allow cross-origin requests and frontend to call API
+app.use(express.json());
 
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from the backend!' });

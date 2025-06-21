2f25aa722119f39e300fa8e253669222417685ac
Laurence Unabia <laurenceunabia@umbradigitalcompany.com>
2025-06-21 23:20:10 +0800

    install axios on the backend

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,29 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
 const app = express();
 const PORT = 4000;
 
 // Allow cross-origin requests and frontend to call API
-app.use(cors({
-     origin: 'http://localhost:3000', // Adjust this to your frontend's URL
-     credentials: true, // Allow credentials if needed
-})); 
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Adjust this to your frontend's URL
+    credentials: true, // Allow credentials if needed
+  })
+);
 app.use(express.json());
 
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from the backend!' });
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get("https://api.sampleapis.com/coffee/hot");
+    const data = response.data;
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
